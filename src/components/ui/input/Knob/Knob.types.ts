0f735e96b02cc468b92c0ff6ae0a716b9e39f5cb
Pedro Gardim <me@pedrogardim.com>
2023-09-19 00:56:18 +0200

    Knob component copied from musa-ui

---
+++ src/components/ui/input/Knob/Knob.types.ts
@@ -0,0 +1,16 @@
+export interface KnobProps {
+  defaultValue?: number;
+  min: number;
+  max: number;
+  size?: number;
+  step?: number;
+  color?: string;
+  label?: string;
+  disabled?: boolean;
+  polar?: boolean;
+  logScale?: boolean;
+  style?: Object;
+  showValue?: boolean;
+  onChange: (value: number) => void;
+  onChangeCommited: (value: number) => void;
+}
