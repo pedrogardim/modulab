500a662965bdf9aeea7c3356397e3bf8a936c2b6
Pedro Gardim <me@pedrogardim.com>
2023-09-19 19:05:58 +0200

    Icon wrapper component

---
+++ src/components/ui/Icon/Icon.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import MDIIcon from "@mdi/react";
+import { IconProps as MDIIconProps } from "@mdi/react/dist/IconProps";
+import { IconName } from "./icons";
+
+interface IconProps extends MDIIconProps {
+  icon: IconName;
+}
+
+const Icon: React.FC<IconProps> = (props) => {
+  const { icon } = props;
+  const [iconPath, setIconPath] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadIcon = async (iconName: IconName) => {
+      const iconPathName = `mdi${
+        iconName.charAt(0).toUpperCase() + iconName.slice(1)
+      }`;
+
+      const icons: Record<string, string> = await import("./icons");
+
+      setIconPath(icons[iconPathName]);
+    };
+    loadIcon(icon);
+  }, [icon]);
+
+  if (!iconPath) return null;
+
+  return <MDIIcon {...props} path={iconPath} />;
+};
+
+export default Icon;
---
+++ src/components/ui/Icon/icons.ts
@@ -0,0 +1,24 @@
+export {
+  mdiCursorDefault,
+  mdiViewGridCompact,
+  mdiRecord,
+  mdiPlus,
+  mdiWaveform,
+  mdiSineWave,
+  mdiTriangleWave,
+  mdiSawtoothWave,
+  mdiSquareWave,
+  mdiVolumeHigh,
+} from "@mdi/js";
+
+export type IconName =
+  | "cursorDefault"
+  | "viewGridCompact"
+  | "record"
+  | "plus"
+  | "waveform"
+  | "sineWave"
+  | "triangleWave"
+  | "sawtoothWave"
+  | "squareWave"
+  | "volumeHight";
