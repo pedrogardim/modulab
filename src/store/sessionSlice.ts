03e0ac2b1b4096cf1d4a8e90aacd6a360febd8e7
Pedro Gardim <me@pedrogardim.com>
2023-09-20 17:41:50 +0200

    sessionSlice: reducers, thunk, state

--- src/store/sessionSlice.ts
+++ src/store/sessionSlice.ts
@@ -1,30 +1,73 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface SessionState {
-  value: number;
+  modules: any[];
+  connections: any[];
 }
 
 const initialState: SessionState = {
-  value: 0,
+  modules: [],
+  connections: [],
 };
+
+export const saveSession = createAsyncThunk(
+  "session/saveSession",
+  async (_, { getState }) => {
+    //TODO: test
+    const { modules, connections } = getState().session;
+    if (!modules) return;
+    localStorage.setItem(
+      "musalabsSession",
+      JSON.stringify({
+        modules: [...modules],
+        connections: [...connections],
+      })
+    );
+  }
+);
 
 export const sessionSlice = createSlice({
   name: "session",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    addModuleToStore: (state, action) => {
+      const { module } = action.payload;
+      state.modules.push(module);
     },
-    decrement: (state) => {
-      state.value -= 1;
+    removeModuleFromStore: (state, action) => {
+      const id = action.payload;
+      state.modules = state.modules.filter((module) => module.id !== id);
+      state.connections = state.connections.filter(
+        (e) => e.module !== id && e.target.module !== id
+      );
     },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    updateModulePosition: (state, action) => {
+      const { moduleIndex, x, y } = action.payload;
+      state.modules[moduleIndex] = {
+        ...state.modules[moduleIndex],
+        x: 32 * Math.round(x / 32),
+        y: 32 * Math.round(y / 32),
+      };
+    },
+    addConnectionToStore: (state, action) => {
+      const connection = action.payload;
+      state.connections.push(connection);
+    },
+    removeConnectionFromStore: (state, action) => {
+      const connectionIndex = action.payload;
+      state.connections = state.connections.filter(
+        (_, i) => i !== connectionIndex
+      );
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = sessionSlice.actions;
+export const {
+  addModuleToStore,
+  removeModuleFromStore,
+  updateModulePosition,
+  addConnectionToStore,
+  removeConnectionFromStore,
+} = sessionSlice.actions;
 
 export default sessionSlice.reducer;
