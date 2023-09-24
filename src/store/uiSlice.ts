76d982f28abb3df1fc7cb3886c17b6cf2be720fc
Pedro Gardim <me@pedrogardim.com>
2023-09-24 09:32:47 +0200

    console.log cleaned

--- src/store/uiSlice.ts
+++ src/store/uiSlice.ts
@@ -31,7 +31,6 @@ export const uiSlice = createSlice({
     },
     toggleConnectionMatrix: (state) => {
       state.connectionMatrixOpen = !state.connectionMatrixOpen;
-      console.log(state.connectionMatrixOpen);
     },
     toggleModuleSelector: (state) => {
       state.moduleSelectorOpen = !state.moduleSelectorOpen;
